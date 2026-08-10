8db89c499b93d2fb0c273b116232ab73405f6318
lexi-blanchet <lexi.blanchet@outlook.com>
2026-08-10 14:12:41 -0600

    Cleanup and dynamic switching depending on deployment method.

--- projects/basic-template/astro.config.mjs
+++ projects/basic-template/astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
 import node from '@astrojs/node';
+
+const isLocal = !process.env.GITHUB_ACTIONS;
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lexi-blanchet.github.io',
-  base: '/astro-portfolios'
-  //server: {host: true},
-  //adapter: node({
-  //  mode: 'standalone'
-  //})
+  base: '/astro-portfolios',
+  server: isLocal ? { host: true } : undefined,
+  adapter: isLocal ? node({ mode: 'standalone' }) : undefined,
 });
