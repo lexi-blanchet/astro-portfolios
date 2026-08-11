f9353829cd84270fbd478009e8326e4b61458937
lexi-blanchet <lexi.blanchet@outlook.com>
2026-08-10 22:38:09 -0600

    Make Static

--- projects/nail-artist/astro.config.mjs
+++ projects/nail-artist/astro.config.mjs
@@ -7,11 +7,6 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
-import node from "@astrojs/node";
-
-const isLocal = !process.env.GITHUB_ACTIONS;
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
 
@@ -21,11 +16,7 @@ export default defineConfig({
       include: ["@keystatic/core", "@keystatic/astro"],
     },
   },
-  output: "server",
-
+  output: "static",
   site: 'https://lexi-blanchet.github.io',
   base: '/astro-portfolios',
-  // Github actions require a specific setup so we exclude our docker config
-  server: isLocal ? { host: true } : undefined,
-  adapter: isLocal ? node({ mode: 'standalone' }) : undefined,
 });
